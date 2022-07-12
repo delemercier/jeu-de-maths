e204cf2e3ea0615fdf36d726f78fde0242b97e96
unknown <delemercier@gmail.com>
2022-07-12 21:49:33 +0200

    denis

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
 
-	import { fade, fly , scale  } from 'svelte/transition';
+	import { fade, fly, scale } from 'svelte/transition';
 	let visible = false;
 	onMount(() => {
 		visible = true;
@@ -12,21 +12,39 @@
 	<div class="grid place-items-center inline-block">
 		{#if visible}
 			<div
-				class="text-6xl w-full text-center bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 p-3"
+				class="text-3xl w-full text-center bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 p-3"
 				in:fade={{ duration: 2000 }}
 			>
 				Joyeux anniversaire Julien !
 			</div>
 		{/if}
-		<div>
+		<div class="text-center">
 			{#if visible}
-				<img src="/images/anniv.png" alt="raté" width="200px" class="inline-block" in:scale ={{ duration: 2000, delay: 1000 }}/>
+				<img
+					src="/images/anniv.png"
+					alt="raté"
+					width="150px"
+					class="inline-block"
+					in:scale={{ duration: 2000, delay: 1000 }}
+				/>
 			{/if}
 			{#if visible}
-				<img src="/images/anniv2.png" alt="raté" width="200px" class="inline-block" in:scale ={{ duration: 2000, delay: 2000 }}/>
+				<img
+					src="/images/anniv2.png"
+					alt="raté"
+					width="150px"
+					class="inline-block"
+					in:scale={{ duration: 2000, delay: 2000 }}
+				/>
 			{/if}
 			{#if visible}
-				<img src="/images/anniv3.png" alt="raté" width="200px" class="inline-block" in:scale ={{ duration: 2000, delay: 3000 }} />
+				<img
+					src="/images/anniv3.png"
+					alt="raté"
+					width="150px"
+					class="inline-block"
+					in:scale={{ duration: 2000, delay: 3000 }}
+				/>
 			{/if}
 		</div>
 
@@ -42,32 +60,20 @@
 		{/if}
 		<div class="h-12" />
 		{#if visible}
-			<div class="text-3xl my-5" in:fade={{ delay: 5000 }}>
-				Je t'offre mon temps et mes compétences pour construire :
-			</div>
-			<div
-				class="text-2xl my-1 text-primary"
-				in:fly={{ delay: 6000, x: -500, duration: 1000 }}
-			>
+			<div class="text-3xl my-1" in:fade={{ delay: 5000 }}>Je t'offre mon temps</div>
+			<div class="text-3xl my-1" in:fade={{ delay: 6000 }}>mes compétences</div>
+			<div class="text-3xl my-1" in:fade={{ delay: 7000 }}>pour construire :</div>
+			<div class="text-2xl my-1 text-primary" in:fly={{ delay: 8000, y: -500, duration: 1000 }}>
 				Des jeux en groupe
 			</div>
-			<div
-				class="text-2xl my-1 text-secondary"
-				in:fly={{ delay: 8000, x: -500, duration: 1000 }}
-			>
+			<div class="text-2xl my-1 text-secondary" in:fly={{ delay: 10000, y: -500, duration: 1000 }}>
 				Des exercices interactifs
 			</div>
-			<div
-				class="text-2xl my-1 text-accent"
-				in:fly={{ delay: 10000, x: 500, duration: 2000 }}
-			>
-				Des trucs qui bougent dans l'autre sens
+			<div class="text-2xl my-1 text-accent" in:fly={{ delay: 12000, y: -500, duration: 5000 }}>
+				Des trucs qui bougent lentement &#128517;
 			</div>
-			<div
-				class="text-2xl my-1 text-info"
-				in:fly={{ delay: 12000, y: -500, duration: 2000 }}
-			>
-				Et même par en haut !
+			<div class="text-2xl my-1 text-info" in:fly={{ delay: 14000, y: -500, duration: 10000 }}>
+				Des trucs qui bougent très lentement &#128529;
 			</div>
 		{/if}
 	</div>
